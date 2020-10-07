6cdde74a85fc6c1e27a0b80c32f0703118a5bbe3
PinkDraconian <vanroeyrobbe@hotmail.com>
2020-10-07 21:32:12 +0200

    Initialized project

---
+++ Exercise2Strategy/Exercise2Strategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise2Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
